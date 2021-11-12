64d993574f43af9b6dd75cb36e85c0b892606983
Roni Oliveira <roni.a.oliveira1@gmail.com>
2021-11-12 16:02:44 -0300

    clean nos imports

--- src/main/java/com/pismo/teste/model/Transacao.java
+++ src/main/java/com/pismo/teste/model/Transacao.java
@@ -1,10 +1,7 @@
 package com.pismo.teste.model;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Objects;
-import java.util.spi.CalendarNameProvider;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
